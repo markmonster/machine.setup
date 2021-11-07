f378ea2b5c2040aa93e07645018a296b0851044d
Mark Monster <mark@monsterconsultancy.nl>
2021-11-07 10:22:35 +0100

    Update InstallApplications.ps1

--- InstallApplications.ps1
+++ InstallApplications.ps1
@@ -1,48 +1,98 @@
-Write-Host @'
-   _____                          __                                                                 
-  /     \   ____   ____   _______/  |_  ___________                                                  
- /  \ /  \ /  _ \ /    \ /  ___/\   __\/ __ \_  __ \                                                 
-/    Y    (  <_> )   |  \\___ \  |  | \  ___/|  | \/                                                 
-\____|__  /\____/|___|  /____  > |__|  \___  >__|                                                    
-        \/            \/     \/            \/                                                        
-_________                            .__   __                                                        
-\_   ___ \  ____   ____   ________ __|  |_/  |______    ____   ____ ___.__.                          
-/    \  \/ /  _ \ /    \ /  ___/  |  \  |\   __\__  \  /    \_/ ___<   |  |                          
-\     \___(  <_> )   |  \\___ \|  |  /  |_|  |  / __ \|   |  \  \___\___  |                          
- \______  /\____/|___|  /____  >____/|____/__| (____  /___|  /\___  > ____|                          
-        \/            \/     \/                     \/     \/     \/\/                               
-__________               __________                                                  ____   ________ 
-\______   \ _______  ___ \______   \_______   ____ ______ ______   ___________       \   \ /   /_   |
- |    |  _//  _ \  \/  /  |     ___/\_  __ \_/ __ \\____ \\____ \_/ __ \_  __ \       \   Y   / |   |
- |    |   (  <_> >    <   |    |     |  | \/\  ___/|  |_> >  |_> >  ___/|  | \/        \     /  |   |
- |______  /\____/__/\_ \  |____|     |__|    \___  >   __/|   __/ \___  >__|            \___/   |___|
-        \/            \/                         \/|__|   |__|        \/                             
+# Generate Text Art
+# https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Monster%0AConsultancy%0AMachine%0ASetup%0AV1.1
+
+Write-Host -ForegroundColor DarkYellow @'
+   _____                          __                                       
+  /     \   ____   ____   _______/  |_  ___________                        
+ /  \ /  \ /  _ \ /    \ /  ___/\   __\/ __ \_  __ \                       
+/    Y    (  <_> )   |  \\___ \  |  | \  ___/|  | \/                       
+\____|__  /\____/|___|  /____  > |__|  \___  >__|                          
+        \/            \/     \/            \/                              
+_________                            .__   __                              
+\_   ___ \  ____   ____   ________ __|  |_/  |______    ____   ____ ___.__.
+/    \  \/ /  _ \ /    \ /  ___/  |  \  |\   __\__  \  /    \_/ ___<   |  |
+\     \___(  <_> )   |  \\___ \|  |  /  |_|  |  / __ \|   |  \  \___\___  |
+ \______  /\____/|___|  /____  >____/|____/__| (____  /___|  /\___  > ____|
+        \/            \/     \/                     \/     \/     \/\/     
+   _____                .__    .__                                         
+  /     \ _____    ____ |  |__ |__| ____   ____                            
+ /  \ /  \\__  \ _/ ___\|  |  \|  |/    \_/ __ \                           
+/    Y    \/ __ \\  \___|   Y  \  |   |  \  ___/                           
+\____|__  (____  /\___  >___|  /__|___|  /\___  >                          
+        \/     \/     \/     \/        \/     \/                           
+  _________       __                                                       
+ /   _____/ _____/  |_ __ ________                                         
+ \_____  \_/ __ \   __\  |  \____ \                                        
+ /        \  ___/|  | |  |  /  |_> >                                       
+/_______  /\___  >__| |____/|   __/                                        
+        \/     \/           |__|                                           
+____   ________     ____                                                   
+\   \ /   /_   |   /_   |                                                  
+ \   Y   / |   |    |   |                                                  
+  \     /  |   |    |   |                                                  
+   \___/   |___| /\ |___|                                                  
+                 \/                                                        
 '@
+Function IsInstalled($id) {
+        #Write-Host $id
+        $installed = winget list -q $id
+        #Write-Host ($installed -clike "*$id*")
+        #$installed | Get-Member
 
+        If($installed -clike "*$id*") {
+                return $true
+        }
+        Else {
+                return $false
+        }
+}
 
-winget install -h -e --id Microsoft.PowerShell
-winget install -h -e --id Microsoft.WindowsTerminal
-winget install -h -e --id Fork.Fork
-winget install -h -e --id Microsoft.VisualStudioCode
-winget install Microsoft.VisualStudio.2019.Professional --silent --override "--wait --quiet --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.NetCrossPlat"
-winget install -h -e --id JetBrains.dotUltimate
-winget install -h -e --id 7zip.7zip
-winget install -h -e --id Typora.Typora
-choco install agentransack
-winget install -h -e --id Microsoft.AzureCLI
-winget install -h -e --id Microsoft.AzureDataStudio
-winget install -h -e --id Bitwarden.Bitwarden
-winget install -h -e --id Docker.DockerDesktop
-winget install -h -e --id Doxie.Doxie
-winget install -h -e --id Elgato.StreamDeck
-winget install -h -e --id Telerik.Fiddler
-winget install -h -e --id Logitech.Options
-choco install logitech-presentation
-winget install -h -e --id Logitech.CameraSettings
-winget install -h -e --id Microsoft.Teams
-winget install -h -e --id 9WZDNCRFJB3H #Microsoft ToDo
-winget install -h -e --id OpenJS.NodeJS
-winget install -h -e --id OBSProject.OBSStudio
-winget install -h -e --id Microsoft.PowerToys
-choco install paint.net
-winget install -h -e --id XSplit.VCam
+Function Install($id) {
+        If(-Not (IsInstalled($id))) {
+                winget install -h -e --id $id
+        }
+        Else {
+                Write-Host -ForegroundColor Green "$id is already installed."
+        }
+}
+
+Install('Microsoft.PowerShell')
+Install('Microsoft.WindowsTerminal')
+Install('Fork.Fork')
+Install('Microsoft.VisualStudioCode')
+If(-Not (IsInstalled('Microsoft.VisualStudio.2019.Professional'))) {
+        winget install Microsoft.VisualStudio.2019.Professional --silent --override "--wait --quiet --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.NetCrossPlat"
+}
+Else {
+        Write-Host -ForegroundColor Green "Microsoft.VisualStudio.2019.Professional is already installed."
+}
+If(-Not (Test-Path "$ENV:LOCALAPPDATA\Jetbrains\Installations")) {
+        winget install -h -e --id 'JetBrains.dotUltimate'
+}
+Else {
+        Write-Host -ForegroundColor Green "JetBrains.dotUltimate is already installed."
+}
+Install('7zip.7zip')
+If(-Not (Test-Path "$ENV:LOCALAPPDATA\Programs\Typora\Typora.exe")) {
+        winget install -h -e --id 'Typora.Typora'
+}
+Else {
+        Write-Host -ForegroundColor Green "Typora.Typora is already installed."
+}
+
+choco install -y agentransack
+Install('Microsoft.AzureCLI')
+Install('Microsoft.AzureDataStudio')
+Install('Bitwarden.Bitwarden')
+Install('Docker.DockerDesktop')
+Install('Doxie.Doxie')
+Install('Elgato.StreamDeck')
+Install('Telerik.Fiddler')
+Install('Logitech.Options')
+Install('Logitech.CameraSettings')
+Install('Microsoft.Teams')
+Install('OpenJS.NodeJS')
+Install('OBSProject.OBSStudio')
+Install('Microsoft.PowerToys')
+choco install -y paint.net
+Install('XSplit.VCam')
